9b56b26f8f1a3ad3635e7945d70302d2010753d4
Remigiusz Pisarski <Remigiusz.pisarski@onet.pl>
2022-03-23 20:35:45 +0100

    finished 3.1 exercise/list 4

--- lab4JezykiS/zadanie3_1/z3_1.bat
+++ lab4JezykiS/zadanie3_1/z3_1.bat
@@ -2,23 +2,26 @@
 
 CHOICE /C PZ /M "Wcisnij P zeby zobaczyc liczbe przypadkow, Z zeby zobaczyc liczbe zgonow."
 SET /P country= Podaj nazwe kraju: 
-SET userChoice = %ERRORLEVEL%
+SET /A userChoice = %ERRORLEVEL%
+DEL "z3A.txt"
 
 FOR /F "TOKENS=1,3,5,6,7" %%A IN (COVID.TXT) DO (
 	IF %%B GEQ %1 (
 		IF %%B LEQ %2 (
 			IF %%E == %country% (
-				IF %userChoice% == "1" (
+				IF %userChoice% == 1 (
+					ECHO %%C %%E %%A >> "z3A.txt"
 					ECHO %%A %%E %%C
 				)ELSE (
+					ECHO %%D %%E %%A >> "z3A.txt"
 					ECHO %%A %%E %%D
-				)
-				
-			)
-			
+				)			
+			)		
 		)
 	)
-)
+) 
 
+sum < z3A.txt
+avr < z3A.txt
 	
 PAUSE
