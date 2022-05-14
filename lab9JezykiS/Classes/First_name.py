4cacec4e27a2758333cc4846e6406d75e2c4302e
remmo <remigiusz.pisarski@onet.pl>
2022-05-14 17:04:21 +0200

    finally works, very very big changes, programm written from start

--- lab9JezykiS/Classes/First_name.py
+++ lab9JezykiS/Classes/First_name.py
@@ -3,39 +3,9 @@ from Errors.NameException import NameException
 
 
 class First_name(Controlled_text):
-    FILEPATH = '/media/remmo/Acer/Uczelnia/Semestr4/Jezyki Skryptowe/laby/lab8JezykiS/PopularneImiona'
-    GENDER_LIST = ([], [])
-
-    @staticmethod
-    def open_file(filename):
-        # wczytujemy tylko gdy tego nie zrobilismy wczesniej
-        if First_name.GENDER_LIST == ([], []):
-            with open(filename, 'r') as f:
-                lines = f.readlines()
-
-                # Wczytywanie imion kobiet
-                i = 1
-                line = lines[i]
-                while line != '\n':
-                    First_name.GENDER_LIST[0].append(line[:-1])
-                    i = i + 1
-                    line = lines[i]
-
-                # Wczytywanie imion meskich
-                i = i + 2
-                line = lines[i]
-                while i < len(lines):
-                    First_name.GENDER_LIST[1].append(line[:-1])
-                    i = i + 1
-                    if i == len(lines):
-                        break
-                    line = lines[i]
-        return First_name.GENDER_LIST
 
     def __init__(self, text):
         super().__init__(text.capitalize())
-        if self.get_text() not in First_name.GENDER_LIST[0] and self.get_text() not in First_name.GENDER_LIST[1]:
-            raise NameException('Imie %s nie wystepuje w pliku PopularneImiona i nie mozna go ustawic!' % text)
 
     @property
     def name(self):
--- lab9JezykiS/main.py
+++ lab9JezykiS/main.py
@@ -1,22 +1,81 @@
-import functools
 import re
-from random import seed, randint
-from typing import Optional
+from random import randint
 
-from fastapi import FastAPI, Path
+import fastapi
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
 
 from Classes.First_name import First_name
-from Classes.Ident_number import Ident_number
 from Classes.Last_name import Last_name
-from Classes.Pearson import Pearson
 
 app = FastAPI()
-PEOPLE = {}
+
+people = {}
 
 
-# generator id
+class Pearson(BaseModel):
+    name: str
+    last_name: str
+    email: str
+    phone_number: str
+
+
+def conversion(pearson: Pearson):
+    pearson.name = First_name(pearson.name).get_text()
+    pearson.last_name = Last_name(pearson.last_name).get_text()
+    if not check_phone_number(pearson.phone_number):
+        raise fastapi.HTTPException(status_code=400, detail="Podany numer telefonu nie jest polski lub poprawny!")
+    if not check_email(pearson.email):
+        raise fastapi.HTTPException(status_code=400, detail="Podany email nie pochodzi z pwr!")
+
+
+@app.post("/people")
+def add_pearson(pearson: Pearson):
+    conversion(pearson)
+    pearson_id = id_generator()
+    if pearson_id in people:
+        raise fastapi.HTTPException(status_code=400, detail="Podana osoba juz istnieje w bazie")
+
+    people[pearson_id] = pearson
+    return people[pearson_id]
+
+
+@app.get("/people")
+def get_everyone():
+    if people:
+        return people
+    else:
+        raise fastapi.HTTPException(status_code=404, detail="Nie ma zadnych osob w bazie!")
+
+
+@app.get("/people/last_name/{last_name}")
+def get_by_last_name(last_name: str = Query(None, title="name")):
+    result = {}
+    for pearson_id in people:
+        if people[pearson_id].last_name == last_name:
+            result[pearson_id] = people[pearson_id]
+    if not result:
+        raise fastapi.HTTPException(status_code=404, detail="Brak osoby o podanym nazwisku w bazie")
+    else:
+        return result
+
+
+@app.get("/people/id/{pearson_id}")
+def get_by_id(pearson_id: str = Path(None, description="Id szukanej osoby")):
+    return people[pearson_id]
+
+
+@app.delete("/people/{pearson_id}")
+def delete_pearson_from_database(pearson_id: str = Query(..., description="Id osoby do usuniecia")):
+    if pearson_id not in people:
+        raise fastapi.HTTPException(status_code=404, detail="Brak osoby o podanym id w bazie")
+
+    del people[pearson_id]
+    return {"Success": "Item deleted!"}
+
+
+# generator numerow id
 def id_generator():
-    seed(1)
     sum_a = 0
     id_n = ''
     for _ in range(7):
@@ -51,129 +110,3 @@ def check_email(address):
             return True
         else:
             return False
-
-
-@app.post("/people")
-def add_person(name: str, last_name: str, email: str, phone_number: str):
-    # utworzenie id
-    new_id = id_generator()
-    while new_id in PEOPLE:
-        new_id = id_generator()
-
-    # sprawdzenie emaila
-    if not check_phone_number(phone_number) or not check_email(email):
-        return {
-            "Error 400": "Znaleziono blad w numerze telefonu lub emailu, prosze upewnic sie ze wprowadzane dane sa "
-                         "prawidlowe"}
-
-    # nie wiem czy to robic
-    First_name.open_file(First_name.FILEPATH)
-
-    # sklejenie danych w jeden tekst
-    ok_name = First_name(name)
-    ok_last_name = Last_name(last_name)
-    data = new_id + ' ' + ok_name.get_text() + ' ' + ok_last_name.get_text()
-
-    # dodanie nowej osoby
-    new_person = Pearson(data)
-    PEOPLE[new_id] = {
-        (new_person, email, phone_number)
-    }
-
-    return {"Pomyslnie dodano osobe"}
-
-@app.get("/people")
-def get_everybody():
-    if PEOPLE:
-        return PEOPLE
-    else:
-        return {"Nie dodales jeszcze zadnej osoby"}
-
-@app.get("/people/{last_name}")
-def get_by_last_name(last_name: str):
-    result = {}
-    for id in PEOPLE:
-        for data in PEOPLE[id]:
-            if data == last_name:
-                result[id] = PEOPLE[id]
-    return result
-
-
-
-
-
-
-
-"""
-
-PEOPLE = {
-    1: {"Nowak", "Kamil"},
-    2: {"Malgorzata", "Wolska"}
-}
-
-
-result = {}
-for id in PEOPLE:
-    for data in PEOPLE[id]:
-        if data == "Nowak":
-            result[id] = PEOPLE[id]
-
-for i in result:
-    print(result)
-
-
-
-inventory = {
-    1: {
-        "name": "Milk",
-        "price": 3.99,
-        "brand": "Regular"
-    }
-}
-
-
-@app.get("/get-item/{item_id}")
-def get_item(item_id: int = Path(None, description="The ID of the item", gt=0, lt=2)):
-    return inventory[item_id]
-
-@app.get("/get-by-name")
-def get_item(name: str):
-    for item_id in inventory:
-        if inventory[item_id]["name"] == name:
-            return inventory[item_id]
-    return {"Data": "Not found"}
-
-
-
-====================== Przyklad z ich strony ====================
-
-from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-# strona do testowania: http://127.0.0.1:8000/docs
-
-app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-"""
